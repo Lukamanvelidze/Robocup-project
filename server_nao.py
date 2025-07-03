61946f5ecefbe46a830b6337000b360e75d2f991
catmub <tuandat.hnvn@gmail.com>
2025-07-03 10:29:54 +0200

    Add files via upload

---
+++ server_nao.py
@@ -0,0 +1,53 @@
+import socket
+import tempfile
+import wave
+import numpy as np
+import whisper
+#run on python3.7
+HOST = '127.0.0.1'
+PORT = 65432
+
+model = whisper.load_model("base")
+
+
+def handle_client(conn):
+    # Receive length of incoming audio
+    length_bytes = conn.recv(4)
+    length = int.from_bytes(length_bytes, byteorder='big')
+
+    data = b''
+    while len(data) < length:
+        packet = conn.recv(4096)
+        if not packet:
+            break
+        data += packet
+
+    # Save to temp wav
+    with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as tmpfile:
+        with wave.open(tmpfile.name, 'wb') as wf:
+            wf.setnchannels(1)
+            wf.setsampwidth(2)
+            wf.setframerate(16000)
+            wf.writeframes(data)
+
+        result = model.transcribe(tmpfile.name)
+
+    # Send transcription back
+    transcription = result["text"].encode('utf-8')
+    trans_len = len(transcription)
+    conn.send(trans_len.to_bytes(4, byteorder='big'))
+    conn.send(transcription)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print("Whisper server running...")
+    while True:
+        print("will accept")
+        conn, addr = s.accept()
+        print("hass accepted")
+        with conn:
+            print(f"Connected by {addr}")
+            handle_client(conn)
+
